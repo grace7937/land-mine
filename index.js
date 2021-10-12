5c0cd058d02e0e16a60e0e4bb6f0a40d2978b968
grace7937 <eunchong.k153@gmail.com>
2021-10-12 22:52:26 +0900

    Add input

---
+++ index.js
@@ -0,0 +1,18 @@
+const row = 10;
+const cell = 10;
+const mine = 10;
+const CODE = {
+
+}
+
+
+
+
+function handleExecution() {
+    const row = parseInt(document.querySelector(".row").value);
+    const cell = parseInt(document.querySelector(".cell").value);
+    const mine = parseInt(document.querySelector(".mine").value);
+
+    console.log(row, cell, mine);
+
+}
