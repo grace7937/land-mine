e1e20648d631f152fe57006d11a8bea9b8f92d91
grace7937 <eunchong.k153@gmail.com>
2021-10-27 23:38:32 +0900

    make 100 arrays

--- index.js
+++ index.js
@@ -5,14 +5,27 @@ const CODE = {
 
 }
 
+let data;
 
 
 
 function handleExecution() {
-    const row = parseInt(document.querySelector(".row").value);
-    const cell = parseInt(document.querySelector(".cell").value);
-    const mine = parseInt(document.querySelector(".mine").value);
-
+    // const row = parseInt(document.querySelector(".row").value);
+    // const cell = parseInt(document.querySelector(".cell").value);
+    // const mine = parseInt(document.querySelector(".mine").value);
+    
     console.log(row, cell, mine);
+    const array = Array(row * cell).fill().map((arr,i) => {
+        return i;
+    })
 
+    console.log(array);
+    
 }
+
+
+
+// 1.배열을 25개 만든다.
+
+
+
